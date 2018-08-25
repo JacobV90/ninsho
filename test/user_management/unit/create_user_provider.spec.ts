1a8ca0b9a957aca47acc27e5ed25edc343c2ad1a
Jacob Veal <jacobveal90@gmail.com>
2018-08-25 18:36:32 -0500

    improved coverage

--- test/user_management/unit/create_user_provider.spec.ts
+++ test/user_management/unit/create_user_provider.spec.ts
@@ -9,15 +9,15 @@ import { Auth0Config, Auth0 } from '../../../src/common/auth0';
 const config: Auth0Config = require('../../../config.json').auth0;
 
 describe('create_user_provider.spec.ts', function () {
+  let userManagement: UserManagementController;
+  before(() => {
+    userManagement = new UserManagementController(new Auth0(config));
+  });
+
   describe('Create User', () => {
     const userOneEmail: string = 'jvtalon901@yahoo.com';
     const userTwoEmail: string = 'testemail@gmail.com';
-    let userManagement: UserManagementController;
     let userId: string;
-
-    before(() => {
-      userManagement = new UserManagementController(new Auth0(config));
-    });
 
     it('should create a new user provided valid parameters', async function () {
       const userData: UserData = {
@@ -101,6 +101,44 @@ describe('create_user_provider.spec.ts', function () {
         expect(error.statusCode).to.equal(400);
         expect(error.message).to.equal('Payload validation error: \'Object didn\'t pass validation for format email: ' +
           userData.email + '\' on property email (The user\'s email).');
+        done();
+      });
+    });
+  });
+
+  describe('Delete User', function () {
+
+    let id: string;
+
+    before(async () => {
+      const user: User = await userManagement.createUser({
+        email: 'vtalon007@yahoo.com',
+        password: 'testPassword123',
+      });
+      if (user.user_id) {
+        id = user.user_id;
+      }
+    });
+
+    it('should delete a user provided a user id', function (done) {
+      userManagement.deleteUser({ id })
+        .then(() => {
+          done();
+        })
+        .catch(() => {
+          expect.fail();
+        });
+    });
+
+    it('should not delete a user provided a user id that does not exist', function (done) {
+      const id: string = 'nope';
+      userManagement.deleteUser({ id })
+      .then(() => {
+        expect.fail();
+      })
+      .catch((error) => {
+        expect(error.statusCode).to.equal(400);
+        expect(error.message).to.equal('Object didn\'t pass validation for format user-id: ' + id);
         done();
       });
     });
